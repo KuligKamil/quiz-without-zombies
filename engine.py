067706c4ccb11a1f81946a0ac9f2a24f7cddcbb2
Kamil Kulig g <kamil.marek.kulig@gmail.com>
2020-05-30 21:50:56 +0200

    Refactor code

--- engine.py
+++ engine.py
@@ -1,38 +1,23 @@
 import string
 import random
-import yaml
 import time
-
-
-from get_data import get_questions_from_api
+from data import get_questions_from_yml, get_questions_from_api
 
 TIME_LIMIT = 15
 time_total = 0
 signs = string.ascii_uppercase[:4]
 points = 0
+questions = get_questions_from_yml('data.yml') + get_questions_from_api()
 
-with open("data.yml", 'r') as stream:
-    try:
-        questions = yaml.safe_load(stream)
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as e:
-        print(e)
-
-questions = questions + get_questions_from_api()
-
-print(questions)
-print(len(questions))
+random.shuffle(questions)
+for question in questions:
+    random.shuffle(question['answers'])
 
 
 def clear(num):
     for _ in range(num):
         print()
 
-
-random.shuffle(questions)
-
-for question in questions:
-    random.shuffle(question['answers'])
 
 clear(80)
 for index, question in enumerate(questions):
@@ -43,16 +28,15 @@ for index, question in enumerate(questions):
     time_elapsed = 0
     is_answer_not_ok = True
     while is_answer_not_ok and time_elapsed < TIME_LIMIT:
-        answer = input("Please enter sign answer: ")
-        print("You entered: " + answer)
+        answer = input('Please enter sign answer: ')
+        print(f'You entered: {answer}')
         if answer in signs:
             is_answer_not_ok = False
 
     time_elapsed = time.time() - time_start
     time_total += time_elapsed
     if time_elapsed > TIME_LIMIT:
-        print('Too late')
-        print(int(time_elapsed), ' sek')
+        print(f'Too late, {int(time_elapsed)} sek')
     else:
         helper = -1
         for i, sign in enumerate(signs):
@@ -66,10 +50,10 @@ for index, question in enumerate(questions):
             print('Wrong answer')
     print(f'Answer time: {int(time_elapsed)} sek')
     if index == len(questions) - 1:
-        input("Please enter to get result")
+        input('Please enter to get result')
     else:
-        input("Please enter to get next question")
+        input('Please enter to get next question')
     clear(80)
 
-print('Result: ', points, '/ ', len(questions))
+print(f'Result: {points} / {len(questions)}')
 print(f'Total time: {int(time_total)} sek')
