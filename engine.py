483d241e8f21a657a3a1416c9e6dc0d3d519926e
Kamil Kulig g <kamil.marek.kulig@gmail.com>
2020-05-30 21:35:42 +0200

    #8 Add total time and time limit

--- engine.py
+++ engine.py
@@ -1,9 +1,15 @@
 import string
 import random
 import yaml
+import time
 
 
 from get_data import get_questions_from_api
+
+TIME_LIMIT = 15
+time_total = 0
+signs = string.ascii_uppercase[:4]
+points = 0
 
 with open("data.yml", 'r') as stream:
     try:
@@ -11,7 +17,9 @@ with open("data.yml", 'r') as stream:
         print(yaml.safe_load(stream))
     except yaml.YAMLError as e:
         print(e)
+
 questions = questions + get_questions_from_api()
+
 print(questions)
 print(len(questions))
 
@@ -20,9 +28,6 @@ def clear(num):
     for _ in range(num):
         print()
 
-
-signs = string.ascii_uppercase[:4]
-points = 0
 
 random.shuffle(questions)
 
@@ -34,23 +39,32 @@ for index, question in enumerate(questions):
     print(question['content'])
     for i, answer in enumerate(question['answers']):
         print(signs[i], answer['content'])
-
+    time_start = time.time()
+    time_elapsed = 0
     is_answer_not_ok = True
-    while is_answer_not_ok:
+    while is_answer_not_ok and time_elapsed < TIME_LIMIT:
         answer = input("Please enter sign answer: ")
         print("You entered: " + answer)
         if answer in signs:
             is_answer_not_ok = False
-    helper = -1
-    for i, sign in enumerate(signs):
-        if sign == answer:
-            helper = i
-    print(question['answers'][helper]['content'])
-    if 'is_correct' in question['answers'][helper]:
-        print('Good answer')
-        points += 1
+
+    time_elapsed = time.time() - time_start
+    time_total += time_elapsed
+    if time_elapsed > TIME_LIMIT:
+        print('Too late')
+        print(int(time_elapsed), ' sek')
     else:
-        print('Wrong answer')
+        helper = -1
+        for i, sign in enumerate(signs):
+            if sign == answer:
+                helper = i
+        print(question['answers'][helper]['content'])
+        if 'is_correct' in question['answers'][helper]:
+            print('Good answer')
+            points += 1
+        else:
+            print('Wrong answer')
+    print(f'Answer time: {int(time_elapsed)} sek')
     if index == len(questions) - 1:
         input("Please enter to get result")
     else:
@@ -58,3 +72,4 @@ for index, question in enumerate(questions):
     clear(80)
 
 print('Result: ', points, '/ ', len(questions))
+print(f'Total time: {int(time_total)} sek')
